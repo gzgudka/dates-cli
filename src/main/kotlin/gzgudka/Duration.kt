f6336a8bbea16ec8a835ff8d5589c58428bfcb26
Grzegorz Zgudka <gzgudka@gmail.com>
2018-08-29 20:15:11 +1000

    Add counting days when dates are passed in the right order

--- src/main/kotlin/gzgudka/Duration.kt
+++ src/main/kotlin/gzgudka/Duration.kt
@@ -1,9 +1,18 @@
 package gzgudka
 
+private val DAYS_IN_MONTH = arrayOf(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+
 class Duration(private val startDate: Date,
                private val endDate: Date) {
 
-    fun elapsedDays() = 0
+    fun elapsedDays(): Int {
+        val daysBeforeStartDate = count(startDate.year(), startDate.month(), startDate.day())
+        val daysBeforeEndDate = count(endDate.year(), endDate.month(), endDate.day())
+        return daysBeforeEndDate - daysBeforeStartDate - 1
+    }
 
+    private fun count(year: Int, month: Int, day: Int) = (year * 365) + daysForMonths(month) + day
+
+    private fun daysForMonths(month: Int) = (0 until month).map { DAYS_IN_MONTH[it] }.sum()
 
 }
--- src/test/kotlin/gzgudka/DurationTest.kt
+++ src/test/kotlin/gzgudka/DurationTest.kt
@@ -14,10 +14,10 @@ class DurationTest(val startDate: Date, val endDate: Date, val daysElapsed: Int)
         @JvmStatic
         @Parameterized.Parameters(name = "{index}: durationBetween[{0}-{1}]={2}")
         fun data(): Collection<Array<Any>> = listOf(
-                arrayOf(Date("07/11/1972"), Date("08/11/1972"), 0)/*,
+                arrayOf(Date("07/11/1972"), Date("08/11/1972"), 0),
                 arrayOf(Date("01/01/2000"), Date("03/01/2000"), 1),
                 arrayOf(Date("02/06/1983"), Date("22/06/1983"), 19),
-                arrayOf(Date("04/07/1984"), Date("25/12/1984"), 173),
+                arrayOf(Date("04/07/1984"), Date("25/12/1984"), 173)/*,
                 arrayOf(Date("03/01/1989"), Date("03/08/1983"), 1979)*/
         )
 
