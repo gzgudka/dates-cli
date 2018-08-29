26b60b040e5970e18a5bfb3ec47c86f1d53eca24
Grzegorz Zgudka <gzgudka@gmail.com>
2018-08-29 20:29:51 +1000

    Add leap years and not worrying about the input dates order

--- src/main/kotlin/gzgudka/Duration.kt
+++ src/main/kotlin/gzgudka/Duration.kt
@@ -1,5 +1,8 @@
 package gzgudka
 
+import kotlin.math.max
+import kotlin.math.min
+
 private val DAYS_IN_MONTH = arrayOf(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
 
 class Duration(private val startDate: Date,
@@ -8,10 +11,12 @@ class Duration(private val startDate: Date,
     fun elapsedDays(): Int {
         val daysBeforeStartDate = count(startDate.year(), startDate.month(), startDate.day())
         val daysBeforeEndDate = count(endDate.year(), endDate.month(), endDate.day())
-        return daysBeforeEndDate - daysBeforeStartDate - 1
+        return (max(daysBeforeStartDate, daysBeforeEndDate) - min(daysBeforeStartDate, daysBeforeEndDate)) - 1
     }
 
-    private fun count(year: Int, month: Int, day: Int) = (year * 365) + daysForMonths(month) + day
+    private fun count(year: Int, month: Int, day: Int) = (year * 365) + countLeapYears(year) + daysForMonths(month) + day
+
+    private fun countLeapYears(year: Int) = (year / 4) - (year / 100) + (year / 400)
 
     private fun daysForMonths(month: Int) = (0 until month).map { DAYS_IN_MONTH[it] }.sum()
 
